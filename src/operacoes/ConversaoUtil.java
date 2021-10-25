328f047e4edf70e6f4325dcd3249f79ec1ece167
leonardo <57204667+japa2002@users.noreply.github.com>
2021-10-25 20:47:56 -0300

    #0009 - Continuação Conversão / Tabela
    Finalizando conversões e validações da tabela

--- src/operacoes/ConversaoUtil.java
+++ src/operacoes/ConversaoUtil.java
@@ -67,5 +67,94 @@ public class ConversaoUtil {
 		}
 		return String.valueOf(soma);
 	}
+	
+	/**
+	 * 
+	 * Conversão de HEXADECIMAL para DECIMAL
+	 *
+	 * @author Leonardo
+	 * @param sNumeroHexa
+	 * @return
+	 * @throws Exception
+	 */
+	public static String converterHexaParaDecimal(String sNumeroHexa) {
+		var valorNegativo = false;
+		if(sNumeroHexa.charAt(0) == '-') {
+			sNumeroHexa = sNumeroHexa.substring(1);
+			valorNegativo = true;
+		}
+		
+		sNumeroHexa = sNumeroHexa.replace("x", "0");
+		
+		var iBaseCalculo = 1;
+		var iVariavelTemporaria = 0;
+		
+		for (int lIdx = sNumeroHexa.length() - 1; lIdx >= 0; lIdx--){
+			
+			switch (sNumeroHexa.toUpperCase().charAt(lIdx)){
+				case '0': iVariavelTemporaria += 0  * iBaseCalculo; break;
+				case '1': iVariavelTemporaria += 1  * iBaseCalculo; break;
+				case '2': iVariavelTemporaria += 2  * iBaseCalculo; break;
+				case '3': iVariavelTemporaria += 3  * iBaseCalculo; break;
+				case '4': iVariavelTemporaria += 4  * iBaseCalculo; break;
+				case '5': iVariavelTemporaria += 5  * iBaseCalculo; break;
+				case '6': iVariavelTemporaria += 6  * iBaseCalculo; break;
+				case '7': iVariavelTemporaria += 7  * iBaseCalculo; break;
+				case '8': iVariavelTemporaria += 8  * iBaseCalculo; break;
+				case '9': iVariavelTemporaria += 9  * iBaseCalculo; break;
+				case 'A': iVariavelTemporaria += 10 * iBaseCalculo; break;
+				case 'B': iVariavelTemporaria += 11 * iBaseCalculo; break;
+				case 'C': iVariavelTemporaria += 12 * iBaseCalculo; break;
+				case 'D': iVariavelTemporaria += 13 * iBaseCalculo; break;
+				case 'E': iVariavelTemporaria += 14 * iBaseCalculo; break;
+				case 'F': iVariavelTemporaria += 15 * iBaseCalculo; break;
+				default: iVariavelTemporaria = 0;
+			}
+			
+			iBaseCalculo *= 16;
+		}
+		
+		if(valorNegativo) {
+			return "-"+iVariavelTemporaria;
+		}
+		return String.valueOf(iVariavelTemporaria);
+	}
+	
+	/**
+	 * 
+	 * Conversão de DECIMAL para HEXADECIMAL 
+	 *
+	 * @author Leonardo
+	 * @param iEntrada
+	 * @return
+	 */
+	public static String converterDecimalParaHexa(int iEntrada){
+		var valorNegativo = false;
+		if(String.valueOf(iEntrada).charAt(0) == '-') {
+			iEntrada = Integer.parseInt(String.valueOf(iEntrada).substring(1));
+			valorNegativo = true;
+		}
+		
+		var sTextoSaida = new StringBuilder();
+		var sHexChars = "0123456789ABCDEF";  
+		
+		var iVariavelTemporaria = 0;
+		
+		while (iEntrada > 16){
+			iVariavelTemporaria = iEntrada % 16;
+			sTextoSaida.insert(0,sHexChars.charAt(iVariavelTemporaria));
+			iEntrada = iEntrada / 16;
+		}
+		
+		if(iEntrada > 0) {
+			sTextoSaida.insert(0,sHexChars.charAt(iEntrada));
+		}
+		
+		if(valorNegativo) {
+			return "-"+sTextoSaida.toString();
+		}
+		return sTextoSaida.toString();
+	}
 
 }
+
